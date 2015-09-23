4be890050eed98d80f9dd2c3c571ba6bc118c8ab
Brian Woodward <brian.woodward@gmail.com>
2015-09-23 08:37:14 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * capture-stream <https://github.com/doowb/capture-stream>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
