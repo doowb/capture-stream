f9f2cf901a7b77ea0cc4828252cb065fa16baf24
Brian Woodward <brian.woodward@gmail.com>
2015-09-28 22:16:13 -0400

    updating example

--- example.js
+++ example.js
@@ -9,3 +9,14 @@ console.log('foo', 'bar');
 var output = restore();
 console.log(output);
 //=> [ [ 'Hello, world!!!\n' ], [ 'foo bar\n' ] ]
+
+restore = capture(process.stdout);
+
+console.log('Hello, world!!!');
+console.log('foo', 'bar');
+
+output = restore(true);
+console.log(output);
+//=> Hello, world!!!
+//=> foo bar
+//=>
